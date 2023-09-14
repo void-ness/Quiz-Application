93d96478b91f3d003203d28349a998a641575f53
void-ness <78547746+void-ness@users.noreply.github.com>
2023-09-14 20:52:10 +0530

    update: home page UI

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -50,33 +50,43 @@ const Home = () => {
     }
 
     return (
-        <div className="flex flex-col w-1/2 mx-auto text-center">
-            <span className="font-bold text-2xl">
-                Welcome!
+        <div className="flex flex-col w-10/12 lg:w-1/2 mx-auto pt-20">
+            <span className="font-bold text-3xl text-center">
+                Quiz Assessment
             </span>
 
-            <div className="flex flex-col my-2">
-                <label htmlFor="username">Username</label>
-                <input id="username" type="text"
-                    placeholder="Enter your Name"
-                    onChange={(e) => setName(e.target.value)}
-                    className="w-1/2 mx-auto"
-                ></input>
+            <div className="mt-10 p-10 bg-green-700/40 rounded-lg mx-auto flex flex-col items-start w-full sm:w-2/3 xl:w-1/2">
+
+                <div className="flex flex-col w-fit">
+                    <label htmlFor="username" className="font-semibold text-lg mb-1">Username</label>
+                    <input id="username" type="text"
+                        placeholder="Enter your Name"
+                        onChange={(e) => setName(e.target.value)}
+                        className="mx-auto px-2 py-1 rounded-md focus-visible:outline-none bg-green-50"
+                    ></input>
+                </div>
+
+                <div className="ques-container flex flex-col mt-5">
+                    <label htmlFor="username" className="font-semibold text-lg mb-1 transition-all ease-in">Questions</label>
+                    {quesId.map((ques, ind) => {
+                        return (
+                            <div key={ind} className="w-fit my-1 hover:font-bold">
+                                <input id={`ques${ind}`} type="checkbox" value={ques} onChange={(e) => handleCheckBox(e.target.value)}
+                                    className=""></input>
+                                <label className="ml-3 text-base" htmlFor={`ques${ind}`}>{ques}</label>
+                            </div>
+                        )
+                    })}
+                </div>
+
+                <button
+                    value={"Submit"}
+                    onClick={handleSubmit}
+                    className="border-2 border-black px-3 py-1 mt-5 hover:font-bold">Submit</button>
             </div>
 
-            <div className="ques-container flex flex-col items-start w-fit mx-auto">
-                {quesId.map((ques, ind) => {
-                    return (
-                        <div key={ind}>
-                            <input id={`ques${ind}`} type="checkbox" value={ques} onChange={(e) => handleCheckBox(e.target.value)}></input>
-                            <label className="ml-3" htmlFor={`ques${ind}`}>{ques}</label>
-                        </div>
-                    )
-                })}
-            </div>
 
 
-            <button value={"Submit"} onClick={handleSubmit} className="border-2 border-black w-fit mx-auto p-1 rounded-lg my-2">Submit</button>
         </div>
     )
 }
